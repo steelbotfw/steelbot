b10604a890fda59fcec1e24e3eb3db3652636c89
Pavel Chebotarev <chebotarevp@gmail.com>
2011-02-28 22:00:29 +0000

    echo command added

--- plugins/default.plugin.php
+++ plugins/default.plugin.php
@@ -34,10 +34,17 @@ function plg_url_dec($val) {
 	S::bot()->Msg("decoded URL = ".urldecode($val)); 
 }	 
 
+function plg_echo($val) {
+    if (!empty($val)) {
+        S::bot()->Msg($val);
+    }
+}
+
+
 S::bot()->RegisterCmd("md5",     "plg_md5",        1,"%c <string> - вычислить md5 хеш строки");
 S::bot()->RegisterCmd("ip2host", "plg_ip2host",    1,"%c <ip> - узнать имя хоста по ip-адресу");
 S::bot()->RegisterCmd("host2ip", "plg_host2ip",    1,"%c <host>- узнать ip-адрес по имени хоста");
 S::bot()->RegisterCmd("urle",    "plg_url_enc",    1,"%c <url> - закодировать URL");
 S::bot()->RegisterCmd("urld",    "plg_url_dec",    1,"%c <encoded url> - раскодировать URL");
-
+S::bot()->RegisterCmd("echo", "plg_echo", 1, "%c - отправить присланную строку");
 
