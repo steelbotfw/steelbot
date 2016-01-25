05d9a4c335e74c2e204a860452e822228fca9888
Pavel Chebotarev <chebotarevp@gmail.com>
2016-01-25 23:37:52 +0300

    fix telegram entitiy construction

--- src/Steelbot/Protocol/Telegram/Entity/Update.php
+++ src/Steelbot/Protocol/Telegram/Entity/Update.php
@@ -29,8 +29,11 @@ class Update
     public function __construct(array $data)
     {
         $this->updateId = $data['update_id'];
-        $this->message = $data['message'] ?? new Message($data['message']);
-        $this->inlineQuery = $data['inline_query'] ?? new InlineQuery($data['inline_query']);
-        $this->chosenInlineResult = $data['chosen_inline_result'] ?? new ChosenInlineResult($data['chosen_inline_result']);
+        $this->message = isset($data['message']) ?
+            new Message($data['message']) : null;
+        $this->inlineQuery = isset($data['inline_query']) ?
+            new InlineQuery($data['inline_query']) : null;
+        $this->chosenInlineResult = isset($data['chosen_inline_result']) ?
+            new ChosenInlineResult($data['chosen_inline_result']) : null;
     }
 }
